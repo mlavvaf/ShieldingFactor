a1494b25342862dbff32e4a8ba6452932e490632
mlavvaf <mdhlvf@gmail.com>
2024-01-11 13:09:34 -0600

    Reproducing paper's results

---
+++ fig3_plot.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 15:33:23 2024
+
+@author: UWTUCANMag
+"""
+
+import numpy as np
+from scipy.linalg import lu, solve
+import matplotlib.pyplot as plt
+
+
+class MatrixSolver:
+    def __init__(self, capm, n, k_values):
+        self.capm = capm
+        self.rank = 2 * capm
+        self.n = n
+        self.k_value = k_value
+        self.sfact_values = []
+        self.r = np.zeros(self.rank)
+        self.t = np.zeros(capm)
+        self.capr1 = np.zeros(capm)
+        self.mur = np.zeros(capm)
+        self.a = np.zeros((self.rank, self.rank))
+        self.inverse = np.zeros((self.rank, self.rank))
+
+    def define_geometry(self):
+        self.capr1[0] = 0.5
+        for i in range(self.capm):
+            self.capr1[i] = self.capr1[0] * (1 + self.k_value) ** i
+
+        for i in range(self.capm):
+            self.t[i] = 1.0 / 16 * 0.0254
+            self.mur[i] = 20000
+            print(f"{i} {self.capr1[i]} {self.t[i]} {self.mur[i]}")
+
+         # Initialize the r array based on capr1 and t
+        j = 0
+        for i in range(self.capm):
+            self.r[j] = self.capr1[i]
+            print(f"r{j} {self.r[j]}")
+            j += 1
+            self.r[j] = self.capr1[i] + self.t[i]
+            print(f"r{j} {self.r[j]}")
+            j += 1
+
+    def fill_matrix(self):
+        for i in range(self.rank):
+            for j in range(self.rank):
+                if j < i:
+                    element = (self.n / (self.n + 1)) * \
+                        (self.r[j] / self.r[i]) ** (2 * self.n + 1)
+                elif j > i:
+                    element = -1
+                else:  # j == i
+                    if (i + 1) % 2 == 1:
+                        m = ((i + 1) + 1) // 2 - 1
+                        element = - \
+                            ((self.n + 1) * self.mur[m] + self.n) / \
+                            ((self.n + 1) * (self.mur[m] - 1))
+                    else:
+                        m = (i + 1) // 2 - 1
+                        element = (
+                            self.n * self.mur[m] + (self.n + 1)) / ((self.n + 1) * (self.mur[m] - 1))
+                self.a[i, j] = element
+                print(f"{element} ", end="")
+            print()
+
+    def print_matrix(self):
+        for i in range(self.rank):
+            for j in range(self.rank):
+                print(f"{self.a[i, j]} ", end="")
+            print()
+
+    def calculate_inverse(self):
+        p, l, u = lu(self.a)
+        self.inverse = solve(u, solve(l, p.T))
+
+        for i in range(self.rank):
+            for j in range(self.rank):
+                print(f"{self.inverse[i, j]} ", end="")
+            print()
+
+    def calculate_sfact(self):
+        summat = 0
+        for i in range(self.rank):
+            sumrow = np.sum(self.inverse[i, :])
+            summat += sumrow
+
+        sfact = 1.0 / (1.0 + summat)
+        self.sfact_values.append(sfact)
+
+
+if __name__ == "__main__":
+    capm = 4
+    k_values = np.linspace(0, 1, 50)  # Vary k from 0 to 1
+    n_values = [1, 2, 3]
+    colors = ['navy', 'purple', 'darkkhaki']
+
+    for n, color in zip(n_values, colors):
+        sfact_values = []
+
+        for k_value in k_values:
+            matrix_solver = MatrixSolver(capm, n, k_value)
+            matrix_solver.define_geometry()
+            matrix_solver.fill_matrix()
+            matrix_solver.calculate_inverse()
+            matrix_solver.calculate_sfact()
+
+            # Plot the result for each iteration
+            plt.plot(k_value, matrix_solver.sfact_values[-1], 'o', color=color)
+    # Set the common labels, legend, and log scale for y-axis outside the loop
+    plt.xlabel('Scale Factor (k)')
+    plt.ylabel('Total Shielding Factor')
+    plt.yscale('log')  # Set y-axis to log scale
+    plt.ylim(100, 1e8)   # Set the y-axis limit from 1 to 10^8
+    plt.grid()
+    legend_handles = [plt.Line2D([0], [0], marker='o', color='w',
+                    markerfacecolor=color, markersize=10) for color in colors]
+    legend_labels = [f'n = {n}' for n in n_values]
+
+    plt.legend(legend_handles, legend_labels)
+    plt.title(
+        'The total shielding factor of four concentric spherical shells')
+    plt.savefig('SphericalShells.png', bbox_inches='tight', dpi=360)
+    plt.show()
---
+++ invert.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 12:44:23 2024
+
+@author: UWTUCANMag
+"""
+
+import numpy as np
+from scipy.linalg import lu, solve
+
+# Define the dimension n of the matrix
+capm = 5  # number of shields.
+rank = 2 * capm  # matrix dimension
+
+n = 1  # multipole order
+# 1 = uniform field
+
+# Define geometry
+r = np.zeros(rank)
+t = np.zeros(capm)
+capr1 = np.zeros(capm)
+
+# Define mu
+mur = np.zeros(capm)
+
+# Define all the used matrices
+a = np.zeros((rank, rank))
+inverse = np.zeros((rank, rank))
+
+# Define parameters of the problem
+capr1[0] = 2.28 / 2  # m
+capr1[1] = 2.4 / 2  # m
+capr1[2] = 2.6 / 2  # m
+capr1[3] = 3.0 / 2  # m
+capr1[4] = 3.5 / 2  # m
+
+t[0] = 0.002  # m
+t[1] = 0.002  # m
+t[2] = 0.003  # m
+t[3] = 0.003  # m
+t[4] = 0.004  # m
+
+for i in range(capm):
+    mur[i] = 20000
+
+for i in range(capm):
+    print(f"{i} {capr1[i]} {t[i]} {mur[i]}")
+
+# Eq. (17)
+j = 0
+for i in range(capm):
+    r[j] = capr1[i]
+    print(f"r{j} {r[j]}")
+    j += 1
+    r[j] = capr1[i] + t[i]
+    print(f"r{j} {r[j]}")
+    j += 1
+
+print("Fill the Matrix")
+
+# Fill the matrix m
+for i in range(rank):
+    for j in range(rank):
+        if j < i:
+            element = (r[j] / r[i]) ** (2 * n)
+        elif j > i:
+            element = -1
+        else:  # j == i
+            if (i + 1) % 2 == 1:
+                m = ((i + 1) + 1) // 2 - 1
+                element = -(mur[m] + 1) / (mur[m] - 1)
+            else:
+                m = (i + 1) // 2 - 1
+                element = (mur[m] + 1) / (mur[m] - 1)
+        a[i, j] = element
+        print(f"{element} ", end="")
+    print()
+
+print("Print the matrix")
+
+# print the matrix
+for i in range(rank):
+    for j in range(rank):
+        print(f"{a[i, j]} ", end="")
+    print()
+
+print("Inverse")
+
+# Make LU decomposition of matrix m
+p, l, u = lu(a)
+
+# Invert the matrix m
+inverse = solve(u, solve(l, p.T))
+
+for i in range(rank):
+    for j in range(rank):
+        print(f"{inverse[i, j]} ", end="")
+    print()
+
+# Act on vector of 1's
+# i.e., add up along a row.
+# then we add up all the rows anyway.
+# just sum the whole matrix, then.
+summat = 0
+for i in range(rank):
+    sumrow = np.sum(inverse[i, :])
+    print(f"{i} sumrow {sumrow}")
+    summat += sumrow
+
+print(f"summat {summat}")
+
+sfact = 1.0 / (1.0 + summat)
+
+print(f"sfact {sfact}")
